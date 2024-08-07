def60baecf62aad1009a480e1e1db454f9de12c0
Rofi Dzaki Abdul Aziz <rofidzakismktibazma@gmail.com>
2024-08-07 14:34:07 +0700

    nomor ketiga

--- no1.js
+++ no1.js
@@ -9,3 +9,12 @@ const nama = "Rofi Dzaki Abdul Aziz";
 const nyapa = (nama) => `WOY,${nama}`
 
 console.log(nyapa("Rofi"))
+
+// no 3 
+const angka = [2,3,4]
+const [dua,tiga,empat] =  angka
+console.log(dua,tiga,empat)
+
+const pengguna = {orang : "Rofi" , umur : 30};
+const {orang , umur} = pengguna
+console.log(orang,umur) 
