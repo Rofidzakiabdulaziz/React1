1b359b801cca074a1500e2efcea5e91ce154181c
Rofi Dzaki Abdul Aziz <rofidzakismktibazma@gmail.com>
2024-08-07 14:39:07 +0700

    nomor keempat

--- no1.js
+++ no1.js
@@ -17,4 +17,19 @@ console.log(dua,tiga,empat)
 
 const pengguna = {orang : "Rofi" , umur : 30};
 const {orang , umur} = pengguna
-console.log(orang,umur) 
+console.log(orang,umur) ;
+
+// no 4 
+const angka1 = [2, 3];
+const angka2 = [...angka1, 4, 5];
+console.log(angka2); // Output: [1, 2, 3, 4]
+
+
+const user1 = { nama: "Rofi", umur: 17 };
+const user2 = { ...user1, kelas: "XII" };
+console.log(user2); // Output: { nama: "Mirza", umur: 30, pekerjaan: "Guru" }
+
+function hitung(...angka) {
+  return angka.reduce((acc, curr) => acc + curr, 0);
+}
+console.log(hitung(3, 2, 3, 4)); // Output: 10
